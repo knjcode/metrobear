d4178aa99de97d282594d985cdd39cff5a94ce3b
knjcode <knjcode@gmail.com>
2014-10-08 01:24:42 +0900

    Add tokyometro constants

---
+++ config/initializers/constants.rb
@@ -0,0 +1,3 @@
+METRO_RAILWAY = YAML.load_file("#{Rails.root}/config/metro_railwayDict.yaml")
+METRO_STATION = YAML.load_file("#{Rails.root}/config/metro_stationDict.yaml")
+METRO_RAILSTA = YAML.load_file("#{Rails.root}/config/metro_railwaystationDict.yaml")
