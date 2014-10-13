550d8ca228b35a67a1daf2b661b643eb6304d5e7
knjcode <knjcode@gmail.com>
2014-10-13 09:21:03 +0900

    Add validation to station and visiting models

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,6 @@
 class Station < ActiveRecord::Base
   has_many :visitings
   has_many :users, through: :visitings
+
+  validates :name, presence: true
 end
--- app/models/visiting.rb
+++ app/models/visiting.rb
@@ -1,4 +1,7 @@
 class Visiting < ActiveRecord::Base
   belongs_to :user
   belongs_to :station
+
+  validates :user_id,    presence: true
+  validates :station_id, presence: true
 end
