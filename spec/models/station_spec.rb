38557e1547cf1432f49536a4cdd0540a12d5d3d8
knjcode <knjcode@gmail.com>
2014-10-13 10:08:35 +0900

    Add tests for station and visiting models

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Station do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @station = Station.new(name: "Ginza")
+  end
+
+  subject { @station }
+
+  it { should respond_to(:name) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @station.name = " " }
+    it { should_not be_valid }
+  end
 end
--- spec/models/visiting_spec.rb
+++ spec/models/visiting_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Visiting do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @visiting = Visiting.new(user_id: 1, station_id: 1)
+  end
+
+  subject { @visiting }
+
+  it { should respond_to(:user_id) }
+  it { should respond_to(:station_id) }
+
+  it { should be_valid }
+
+  describe "when user_id is not present" do
+    before { @visiting.user_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when station_id is not present" do
+    before { @visiting.station_id = nil }
+    it { should_not be_valid }
+  end
 end
