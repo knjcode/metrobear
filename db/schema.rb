4651dc69edca6ec23c7c4da726baf291741424aa
knjcode <knjcode@gmail.com>
2014-11-06 00:20:15 +0900

    Remove trophies table from schema

--- db/schema.rb
+++ db/schema.rb
@@ -19,12 +19,6 @@ ActiveRecord::Schema.define(version: 20141104022728) do
     t.datetime "updated_at"
   end
 
-  create_table "trophies", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
