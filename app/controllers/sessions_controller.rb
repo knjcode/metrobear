3da57cecb62f9cdf06d6add77efd240941620244
knjcode <knjcode@gmail.com>
2014-11-06 00:37:19 +0900

    Reset trophies at login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
     user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
     session[:user_id] = user.id
     flash[:success] = 'ログインしました'
+    user.set_trophy
     redirect_to root_path
   end
 
