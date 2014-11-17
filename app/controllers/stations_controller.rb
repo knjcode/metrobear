85249db7bc3abebc263dba385ef025b73ee9fa0a
knjcode <knjcode@gmail.com>
2014-11-17 21:03:48 +0900

    Modify style

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -4,7 +4,7 @@ class StationsController < ApplicationController
 
   def show
     @station = Station.find_by_id(METRO_STATION.keys.find_index(params[:id])+1)
-    @comments = @station.comments.paginate(page: params[:page])
+    @comments = @station.comments.paginate(page: params[:page], per_page: 10)
     @comment = current_user.comments.build if signed_in?
   end
 end
